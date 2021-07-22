33fe407d15854cec3b5ee085e6d81cefd4489d91
zhendi <zhen.di.wang@gmail.com>
2021-07-22 12:17:48 +0800

    refactor: move developers dir outside

--- deploy.production.sh
+++ deploy.production.sh
@@ -3,13 +3,10 @@
 rm -rf ./dist
 mkdir dist
 
-cd developers
 yarn install && yarn build
-cd ..
-mv developers/dist dist/developers
 
-SUM=`md5 -q dist/developers/index.html`
-mv dist/developers/index.html dist/developers/index.$SUM.html
+SUM=`md5 -q dist/index.html`
+mv dist/index.html dist/index.$SUM.html
 
 cp app.one.yaml dist/app.yaml
 sed -i ''  "s/index.html/index.$SUM.html/g" dist/app.yaml || exit
--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,8 @@ mkdir dist/api
 #mv api/_site/assets api/_site/scripts api/_site/images api/_site/search api/_site/favicon.ico api/_site/apple-touch-icon.png api/_site/launcher.png api/_site/robots.txt api/_site/index.html dist/api/
 mv api/_site/* dist/api/
 
-cd developers
 yarn install
 yarn build
-cd ..
-mv developers/dist dist/developers
 
 rsync -rcv dist/* one@mixin-developers:html/
 git reset --hard
